b938a2dfebad94c1b56055ee2e2fe01a73f17e3a
Saakshaat <saakshaat2001@gmail.com>
2020-05-24 16:28:23 -0400

    Adding controller and Views for Organizations and a Homepage

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,5 @@
 class Organization < ApplicationRecord
     validates :name, presence: true, length: { minimum: 5 }
     validates :description, presence: true, length: { minimum: 20 }
+    validates :size, numericality: { only_integer: true, greater_than_or_equal_to: 1}
 end
