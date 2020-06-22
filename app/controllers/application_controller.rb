ebbb7032430b2ef424d3d3876873465631b87077
Saakshaat <saakshaat2001@gmail.com>
2020-06-21 17:52:20 -0700

    updating devise setup in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   protected
-  
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
