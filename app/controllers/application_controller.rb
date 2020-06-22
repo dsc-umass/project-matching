97d3b5859eea608102ca2d7d6a5c1c3e8242e0dc
Saakshaat <saakshaat2001@gmail.com>
2020-06-21 17:58:37 -0700

    changing for to permit in devise setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   protected
-
+  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up) << :name
+    devise_parameter_sanitizer.permit(:account_update) << :name
   end
 end
