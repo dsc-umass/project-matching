4f4eea317f2f8a9a447e4c5bf48e37f3c5f13873
Atrik Das <atrikdas@gmail.com>
2020-06-24 17:22:56 +0000

    Generated basic scaffold for Positions

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_170800) do
+ActiveRecord::Schema.define(version: 2020_06_24_172158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "positions", force: :cascade do |t|
+    t.string "title"
+    t.integer "project_id"
+    t.string "user_id"
+    t.string "integer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
