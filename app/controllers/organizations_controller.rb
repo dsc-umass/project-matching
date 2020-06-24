be472e67dac4186d305ad89d67b2efae4268590f
Saakshaat <saakshaat2001@gmail.com>
2020-06-24 11:51:51 -0700

    adding tags

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -69,6 +69,6 @@ class OrganizationsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def organization_params
-      params.require(:organization).permit(:name, :email)
+      params.require(:organization).permit(:name, :email, :tag_list)
     end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,6 @@
 class Organization < ApplicationRecord
-    # has_one :admin
+    # has_one :admin   
+    acts_as_taggable_on :tags
+    acts_as_taggable_on :skills, :interests 
     has_and_belongs_to_many :users
 end
