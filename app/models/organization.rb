3e0e8f5173659a9bc1ec059d5ae626ec9020cdde
Saakshaat <saakshaat2001@gmail.com>
2020-06-28 18:50:44 -0700

    changing tags on organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,7 +1,7 @@
 class Organization < ApplicationRecord
     # has_one :admin   
     acts_as_taggable_on :tags
-    acts_as_taggable_on :skills, :interests 
+    acts_as_taggable_on :type
     has_and_belongs_to_many :users
     has_many :projects
 end
