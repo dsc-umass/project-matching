31dae4dc9aeec879e85c04539030687325576c3e
Saakshaat <saakshaat2001@gmail.com>
2020-06-28 20:45:55 -0700

    adding skills and interests tags to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  acts_as_taggable_on :skills, :interests
   devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
   has_and_belongs_to_many :organizations
