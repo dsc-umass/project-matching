606743d7867a39d0b2c1758b9ae89090b398f568
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-06-29 22:29:52 -0400

    bash scripts and docker file

---
+++ docker/docker_run.sh
@@ -0,0 +1,3 @@
+ 
+#!/bin/bash
+docker-compose run -v `pwd`:/usr/src/app -p 3000:3000 rails "$@"
---
+++ docker/docker_set_up.sh
@@ -0,0 +1,11 @@
+# Work around for the pesky "server is already running bug"
+rm tmp/pids/server.pid
+
+if [ \( ! -f "db/postgres/.built" \) ]; then
+  echo Development db or test db does not exist. Running initial setup... &&
+  bundle exec rake db:create  &&
+  bundle exec rake db:migrate  &&
+  bundle exec rake db:setup
+  touch db/postgres/.built
+fi
+bundle exec rails server -b 0.0.0.0
