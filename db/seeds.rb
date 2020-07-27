a37314703c8e368efddf05d9ee10b1e0c3dc2cc3
Saakshaat <saakshaat2001@gmail.com>
2020-07-26 22:40:57 -0700

    Initialize seed file with organizations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+organization_list = [
+    ["UMass ACM", "umass.acm@gmail.com"],
+    ["UMass DSC", "umass.dsc@gmail.com"],
+    ["UIUC DSC", "uiuc.dsc@gmail.com"],
+    ["NTU DSC", "ntu.dsc@gmail.com"],
+    ["Linux Org.", "linux@linux.org"]
+]
+
+organization_list.each do |name, email|
+  Organization.create(name: name, email: email)
+end
