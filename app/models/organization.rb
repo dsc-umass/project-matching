e8e44f06f921c2d5fcd526f50befcdfb06fa9a36
Saakshaat <saakshaat2001@gmail.com>
2020-08-01 22:05:17 -0700

    Add deeper queries in Organization model for returning CSVs

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,22 +1,35 @@
 require 'csv'
 
 class Organization < ApplicationRecord
-    # has_one :admin
-    acts_as_taggable_on :types
-    has_and_belongs_to_many :users
-    has_many :projects
-    validates :name, presence: true
-    validates :email, presence: true
-    def self.to_csv
-        attributes = %w{id name email projects}
+  # has_one :admin
+  acts_as_taggable_on :types
+  has_and_belongs_to_many :users
+  has_many :projects
+  validates :name, presence: true
+  validates :email, presence: true
 
-        CSV.generate(headers: true) do |csv|
-            csv << attributes
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << ["Name", "Email", "Projects: Positions", "Users"]
 
-            self.find_each do |organization|
-                csv << attributes.map{ |attr| organization.send(attr) }
-            end
+      all.each do |organization|
+        projects = nil
+        users = nil
+        row = [organization.name, organization.email]
+        # if organization.users.any?
+        #   users = organization.users.collect{|u| "#{u.first_name} #{u.email}"}.join(", ")
+        #   row << users
+        # end
+        if organization.projects.any?
+          all_projects = organization.projects
+          projects = []
+          all_projects.each do |project|
+            projects.append("#{project.title}: [#{project.positions.collect { |p| p.title }.join(", ")}]  ")
+          end
+          projects = projects.join(", ")
         end
+        csv << [organization.name, organization.email, projects]
+      end
     end
-
+  end
 end
