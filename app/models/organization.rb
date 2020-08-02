845f8d0deac7ef47ce1524a6ce7b7fe97d64ed2c
Saakshaat <saakshaat2001@gmail.com>
2020-08-01 18:46:41 -0700

    Update Organization model with method for returning CSV of organizations data

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,8 +1,19 @@
 class Organization < ApplicationRecord
-    # has_one :admin   
+    # has_one :admin
     acts_as_taggable_on :types
     has_and_belongs_to_many :users
     has_many :projects
     validates :name, presence: true
     validates :email, presence: true
+    def self.to_csv
+        attributes = %w{id name email projects}
+
+        CSV.generate(headers: true) do |csv|
+            csv << attributes
+
+            all.each do |organization|
+                csv << attributes.map{ |attr| organization.send(attr) }
+            end
+        end
+    end
 end
